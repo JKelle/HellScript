973f471363df660e370c82aa9c79433669dabd03
Josh Kelle <joshua.a.kelle@gmail.com>
2016-04-14 19:21:39 -0500

    added four sample files

---
+++ samples/func.scala
@@ -0,0 +1,15 @@
+
+def factorial(n:int) {
+	if(n == 1) {
+		return 1
+	}
+	return n*factorial(n-1)
+}
+
+def area(w:int, h:width) {
+	var a:int = w*h
+	return a
+}
+
+print(factorial(4))
+print(area(20,3))
