e49a7a11a681a7f7d25c27f652e15946b2b5022f
Dallas Kelle <dkelle@wireless-10-147-144-143.public.utexas.edu>
2016-04-14 19:40:24 -0500

    We can now print using hell script

---
+++ src/HellScript.scala
@@ -0,0 +1,17 @@
+class HellScript {
+    
+    var vars:Map[Symbol,Int] = Map()
+    
+    def print(str:String) {
+	println(str);
+    }
+
+    def print(sym:Symbol) {
+	println(vars(sym));
+    }
+   
+    def SET(sym:Symbol, value:Int) {
+        vars += sym -> value;
+    }
+
+}
--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,8 +1,5 @@
-object HelloWorld extends TypeScript{
+object HelloWorld extends HellScript{
   def main(args: Array[String]): Unit = {
-    10 PRINT "hello world"
-    20 PRINT "on line two"
-    30 SET ('a, 2)
-    40 PRINT 'a
+    print("helloworld");
   }
 }
