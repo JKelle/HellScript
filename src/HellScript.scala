eac50aea64ca1018b8a8393defc25ec06e922b55
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-18 20:25:52 -0500

    Updated set function and added get function
    The get function isn't working properly at the moment, I'm not sure why

--- src/HellScript.scala
+++ src/HellScript.scala
@@ -1,20 +1,20 @@
 class HellScript {
-
+    
     var ints:Map[Symbol,Int] = Map()
     var strings:Map[Symbol, String] = Map()
-
+    
     def print(str:String) {
-        println(str);
+	      println(str);
     }
 
     def print(sym:Symbol) {
-        if(strings contains sym)
-            println(strings(sym));
-        else if(ints contains sym)
-            println(ints(sym))
+	      if(strings contains sym)
+	           println(strings(sym));
+	      else if(ints contains sym)
+	           println(ints(sym))
     }
-
-    def SET(sym:Symbol, value:Int) {
+   
+    def set(sym:Symbol, value:Int) {
         ints += sym -> value
         
         // To use when we're ready to make things hell-ish
@@ -25,18 +25,30 @@ class HellScript {
 //        }
     }
     
-
+    def set(sym:Symbol, value:String) {
+        strings += sym -> value
+    }
+    
+    // TODO: Basically returns nothing
+    def get(sym:Symbol) {
+        if(strings contains sym)
+	           return strings(sym)
+	      else if(ints contains sym)
+	           return ints(sym)
+    }
+    
+    
     abstract sealed class HellScriptClass
     case class setVar(sym:Symbol) extends HellScriptClass{
         object PRINT {
             def apply(str:String) = set(sym, str);
             def apply(v:Int) = set(sym,v);
         }
-
+        
         def set(sym:Symbol, v:Int) {
             ints += sym -> v
         }
-
+        
         def set(sym:Symbol, v:String) {
             strings += sym -> v
         }
