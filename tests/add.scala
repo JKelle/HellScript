e4097a9449316e93b47db040ed20ba9636878927
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-19 20:02:33 -0500

    Updated tests to run with current implementation of HellScript
    The syntax for these tests in only temporary.

--- tests/add.scala
+++ tests/add.scala
@@ -3,8 +3,15 @@ object Add extends HellScript{
     def main(args: Array[String]): Unit = {
         set('x, 5)
         print('x)
-        set('y, get('x))
-        set('x, ('y + 1))
+        set('x, (getInt('x) + 1))
         print('x)
+        set('y, "hello")
+        print('y)
+        
+        // Original syntax:
+//        var x:Int = 5
+//        print(x)
+//        x = x - 1
+//        print(x)
     }
 }
