f44ccdc53f826a33edb7f13d041255a14ff38833
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-19 20:06:48 -0500

    Added new print method, modified the set method, and added getInt and getString methods
    It should be possible to make one get method that has multiple possible return types, this is something we should look into, or maybe ask Jacob about

--- src/HellScript.scala
+++ src/HellScript.scala
@@ -4,54 +4,59 @@ class HellScript {
     var strings:Map[Symbol, String] = Map()
     
     def print(str:String) {
-	      println(str);
+	      println(str)
+    }
+    
+    def print(value:Int) {
+        println(value)
     }
 
     def print(sym:Symbol) {
 	      if(strings contains sym)
-	           println(strings(sym));
+	           println(strings(sym))
 	      else if(ints contains sym)
 	           println(ints(sym))
     }
    
-    def set(sym:Symbol, value:Int) {
-        ints += sym -> value
-        
+    def set(sym:Symbol, value:Any) {
         // To use when we're ready to make things hell-ish
-//        if (sym.name.charAt(0) == 't') {
-//          vars += sym -> value
-//        } else {
-//          throw new Exception("Variables must begin with 't'");
-//        }
+//        if (sym.name.charAt(0) != 't')        
+//          throw new Exception("You fool! Variables must begin with 't'");
+        
+        value match {
+          case value:Int => ints += sym -> value
+          case value:String => strings += sym -> value
+        }
     }
     
-    def set(sym:Symbol, value:String) {
-        strings += sym -> value
+    def getInt(sym:Symbol) : Int = {
+        if(ints contains sym)
+	          ints(sym)
+	      0
     }
     
-    // TODO: Basically returns nothing
-    def get(sym:Symbol) {
+    def getString(sym:Symbol) : String = {
         if(strings contains sym)
-	           return strings(sym)
-	      else if(ints contains sym)
-	           return ints(sym)
+	          strings(sym)
+	      ""
     }
     
     
-    abstract sealed class HellScriptClass
-    case class setVar(sym:Symbol) extends HellScriptClass{
-        object PRINT {
-            def apply(str:String) = set(sym, str);
-            def apply(v:Int) = set(sym,v);
-        }
-        
-        def set(sym:Symbol, v:Int) {
-            ints += sym -> v
-        }
-        
-        def set(sym:Symbol, v:String) {
-            strings += sym -> v
-        }
-    }
-    implicit def setVars(sym:Symbol) = setVar(sym)
+//    abstract sealed class HellScriptClass
+//    case class setVar(sym:Symbol) extends HellScriptClass{
+//        object PRINT {
+//            def apply(str:String) = set(sym, str);
+//            def apply(v:Int) = set(sym,v);
+//        }
+//        
+//        def set(sym:Symbol, v:Int) {
+//            ints += sym -> v
+//        }
+//        
+//        def set(sym:Symbol, v:String) {
+//            strings += sym -> v
+//        }
+//    }
+//    implicit def setVars(sym:Symbol) = setVar(sym)
+    
 }
