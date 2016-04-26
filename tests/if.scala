696e76092be3fe6ff45ddebc288915b0b5cd9407
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-26 11:53:24 -0500

    Slight update to tests

--- tests/if.scala
+++ tests/if.scala
@@ -1,32 +1,16 @@
 object If extends HellScript {
     def main(args: Array[String]): Unit = {
-
         'x := 1
         'y := 5
-
+        
         If('x > 'y) {
             Println("x")
         }
-        Elseif ('y > 'x) {
+        ElseIf('y > 'x) {
             Println("y")
         }
         Else {
             Println("z")
         }
-
-        // Original syntax:
-//        var x:int = 1
-//        var y:int = 5
-//
-//        if (x > y) {
-//            print("x")
-//        }
-//        else if (y > x) {
-//            print("y")
-//        }
-//        else {
-//            print("xy")
-//        }
-
     }
 }
