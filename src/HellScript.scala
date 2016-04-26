b812a3f33c7f5c5469d32fb616bfb0573580152e
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-26 11:56:40 -0500

    Slight change to If statements

--- src/HellScript.scala
+++ src/HellScript.scala
@@ -1,45 +1,43 @@
-import scala.language.implicitConversions
 class HellScript {
     
     var ints:Map[Symbol,Int] = Map()
     var strings:Map[Symbol, String] = Map()
-    var condition:Boolean = false
+    var condition:Boolean = false;
     var funcs:Map[Symbol, () => Unit] = Map()
 
-  case class Assign(v: Symbol){
-      def :=(value:Int)= Set(v,value)
-      def :=(value:String)= Set(v,value)
-  }
-  case class LogicOp(lhs: Boolean){
-      def or(rhs:Boolean)= lhs || rhs
-      def and(rhs:Boolean)= lhs && rhs
-  }
-
-
-
-  def not(value: Boolean): Boolean = {
-    !value
-  }
-
-  implicit def inttobool(i: Int):Boolean = if(i==0) false else true
-  //implicit def vartostr(s:Symbol) = GetString(s)
-  implicit def vartoint(s:Symbol) = GetInt(s)
-  implicit def varname(sym: Symbol) = Assign(sym)
-  implicit def logic(b:Boolean) = LogicOp(b)
-  implicit def logic(i:Int) = LogicOp(i)
-
-
-  def Print(value:Int) {
+    case class Assign(v: Symbol){
+        def :=(value:Int)= Set(v,value)
+        def :=(value:String)= Set(v,value)
+    }
+    case class LogicOp(lhs: Boolean){
+        def or(rhs:Boolean)= lhs || rhs
+        def and(rhs:Boolean)= lhs && rhs
+    }
+  
+  
+  
+    def not(value: Boolean): Boolean = {
+      !value
+    }
+  
+    implicit def inttobool(i: Int):Boolean = if(i==0) false else true
+    //implicit def vartostr(s:Symbol) = GetString(s)
+    implicit def vartoint(s:Symbol) = GetInt(s)
+    implicit def varname(sym: Symbol) = Assign(sym)
+    implicit def logic(b:Boolean) = LogicOp(b)
+    implicit def logic(i:Int) = LogicOp(i)
+    
+    def Print(value:Int) {
         print(value)
     }
     
     def Print(str:String) {
-        print(str)
+        print(str);
     }
 
     def Print(sym:Symbol) {
         if(strings contains sym)
-            print(strings(sym))
+            print(strings(sym));
         else if(ints contains sym)
             print(ints(sym))
     }
@@ -49,31 +47,31 @@ class HellScript {
     }
     
     def Println(str:String) {
-        println(str)
+        println(str);
     }
 
     def Println(sym:Symbol) {
         if(strings contains sym)
-            println(strings(sym))
+            println(strings(sym));
         else if(ints contains sym)
             println(ints(sym))
     }
     
-    def run(body: => Unit) {
-        body
-    }
-
     def If(pred:Boolean)(body: => Unit) {
         if (pred) {
             body
             condition = true
+        } else {
+            condition = false
         }
     }
     
-    def Elseif(pred:Boolean)(body: => Unit) {
+    def ElseIf(pred:Boolean)(body: => Unit) {
         if (pred & !condition) {
             body
             condition = true
+        } else {
+            condition = false
         }
     }
     
@@ -83,18 +81,18 @@ class HellScript {
         
         condition = false
     }
+    
 
 /*    def Def(funcname: Symbol)(body : => Unit) {  
         funcs += funcname -> body;
         println("in def") 
         println(body) 
-
     }*/
 
     def Def(funcname: Symbol)(arg1:Any)(body : () => Unit) {
         funcs += funcname -> body
-    } 
-
+    }
+    
     def Set(sym:Symbol, value:Any) {
         // To use when we're ready to make things hell-ish
 //        if (sym.name.charAt(0) != 't')        
@@ -108,16 +106,16 @@ class HellScript {
     
     def GetInt(sym:Symbol) : Int = {
         if(ints contains sym)
-	           ints(sym)
-         else 0
+	          return ints(sym)
+	      return 0
     }
     
     def GetString(sym:Symbol) : String = {
         if(strings contains sym)
-	          strings(sym)
-        else ""
+	          return strings(sym)
+	      return ""
     }
-
+    
     def temp(sym:Symbol) {
         println(sym)
         println(funcs contains sym)
@@ -138,10 +136,8 @@ class HellScript {
         }
         else if( ints contains sym) {
             ints(sym);
-
           }*/
     }
     implicit def funccall(symbol:Symbol) = funcs(symbol)
-    
     
 }
