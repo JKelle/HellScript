3ff73c78dc26339d857a96107a3dfc1314ff0ac9
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-28 13:53:10 -0500

    Updated Add.scals to show off exceptions

---
+++ tests/Add.scala
@@ -0,0 +1,47 @@
+object Add extends HellScript{
+    def main(args: Array[String]): Unit = {
+        // Begin here
+        'variableX := 2
+        Println('variableX)
+        'variableX := 'variableX + 1
+        Println('variableX)
+        'variableY := "a string"
+        Println('variableY)
+//        
+//        'varX := 2
+//        Println('tX)
+//        'varX := 'varX + 1
+//        Println('varX)
+//        'varY := "a string"
+//        Println('varY)
+//        
+//        'vrX := 2
+//        Println('vrX)
+//        'vrX := 'vrX + 1
+//        Println('vrX)
+//        'vrY := "a string"
+//        Println('vrY)
+//        
+//        'tX := 2
+//        Println('tX)
+//        'tX := 'tX + 1
+//        Println('tX)
+//        'tY := "a string"
+//        Println('tY)
+//        
+//        'tx := 1
+//        Println('tx)
+//        'tx := 'tx + 2
+//        Println('tx)
+//        'ty := "a string"
+//        Println('ty)
+//        
+        // End here
+//        'tx := 1
+//        Println('tx)
+//        'tx := 'tx + 2
+//        Println('tx)
+//        'ty := "str"
+//        Println('ty)
+    }
+}
