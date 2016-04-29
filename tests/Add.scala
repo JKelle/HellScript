59f7bab922a190301e98ef9a96121abf69269753
Collin Massey <Czmassey@users.noreply.github.com>
2016-04-29 02:51:13 -0500

    Updated test files to show off restrictions
    In each file, sections are commented out. In order to show off the restrictions we need to progress through each section, starting from the top. We don't need to keep the commented out code, that's just to show the general progression we should be doing.
    Func.scala is unfinished.

--- tests/Add.scala
+++ tests/Add.scala
@@ -1,47 +1,30 @@
 object Add extends HellScript{
     def main(args: Array[String]): Unit = {
         // Begin here
-        'variableX := 2
-        Println('variableX)
+        'variableX := 0
+        Print('variableX)
         'variableX := 'variableX + 1
-        Println('variableX)
-        'variableY := "a string"
-        Println('variableY)
-//        
-//        'varX := 2
-//        Println('tX)
+        Print('variableX)
+        
+//        'varX := 0
+//        Print('tX)
 //        'varX := 'varX + 1
-//        Println('varX)
-//        'varY := "a string"
-//        Println('varY)
-//        
-//        'vrX := 2
-//        Println('vrX)
-//        'vrX := 'vrX + 1
-//        Println('vrX)
-//        'vrY := "a string"
-//        Println('vrY)
-//        
-//        'tX := 2
-//        Println('tX)
-//        'tX := 'tX + 1
-//        Println('tX)
-//        'tY := "a string"
-//        Println('tY)
-//        
-//        'tx := 1
-//        Println('tx)
-//        'tx := 'tx + 2
-//        Println('tx)
-//        'ty := "a string"
-//        Println('ty)
-//        
+//        Print('varX)
+        
+//        'x := 0
+//        Print('x)
+//        'x := 'x + 1
+//        Print('x)
+        
+//        'tx := 0
+//        Print('tx)
+//        'tx := 'tx + 1
+//        Print('tx)
+        
         // End here
 //        'tx := 1
-//        Println('tx)
+//        Print('tx)
 //        'tx := 'tx + 2
-//        Println('tx)
-//        'ty := "str"
-//        Println('ty)
+//        Print('tx)
     }
 }
