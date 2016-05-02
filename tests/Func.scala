8eed7777039e0e176dd8256360cc93fc273cbf5a
Dallas Kelle <dkelle@wireless-10-147-104-185.public.utexas.edu>
2016-05-02 14:45:03 -0500

    adding func

--- tests/Func.scala
+++ tests/Func.scala
@@ -1,18 +1,16 @@
 object Func extends HellScript {
-//  	Def factorial('n) = {
-//  		Else('n == 1) {
-//  			return 1
-//  		}
-//  		return 'n * factorial('n - 1)
-//  	}
-
-//  	Def area(w, h) = {
-//  		'a := w * h
-//  		return 'a
-//  	}
-
     def main(args: Array[String]): Unit = {
-//    		Print(factorial(5))
-//    		Print(area(21,3))
+        Def noarg() = {
+            Print("No args!")
+        }
+
+        Def onearg('thishasonearg) = {
+            Print('thishasonarg)
+        }
+
+        noarg()
+        'tx := 5
+        'tx := 'tx + 1
+        onearg('tx)
     }
 }
