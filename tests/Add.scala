0350b5f2dddfb26c0c51ac85c9cf2f678e612bd3
Collin Massey <Czmassey@users.noreply.github.com>
2016-05-02 14:16:08 -0500

    Updated tests with text files to describe steps

--- tests/Add.scala
+++ tests/Add.scala
@@ -5,26 +5,5 @@ object Add extends HellScript{
         Print('variableX)
         'variableX := 'variableX + 1
         Print('variableX)
-        
-//        'varX := 0
-//        Print('tX)
-//        'varX := 'varX + 1
-//        Print('varX)
-        
-//        'x := 0
-//        Print('x)
-//        'x := 'x + 1
-//        Print('x)
-        
-//        'tx := 0
-//        Print('tx)
-//        'tx := 'tx + 1
-//        Print('tx)
-        
-        // End here
-//        'tx := 1
-//        Print('tx)
-//        'tx := 'tx + 2
-//        Print('tx)
     }
 }
--- tests/HelloWorld.scala
+++ tests/HelloWorld.scala
@@ -1,14 +1,6 @@
 object HelloWorld extends HellScript{
     def main(args: Array[String]): Unit = {
-        // Start here
-        Println("hello")
-        Println("world")
-        
-//        Println("h3ll0")
-//        Println("w0rld")
-        
-        // End here
-//        Print("h3ll0")
-//        Print("w0rld")
+        Print("h3ll0")
+        Print("w0rld")
     }
 }
